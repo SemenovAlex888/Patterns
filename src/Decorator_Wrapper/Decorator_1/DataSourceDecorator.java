07912ed067d121da8681437f0fe1a9451d1b724d
Alexey Semenov <aurum25@ya.ru>
2021-09-03 21:43:07 +0300

    add Decorator

---
+++ src/Decorator_Wrapper/Decorator_1/DataSourceDecorator.java
@@ -0,0 +1,25 @@
+package Decorator_Wrapper.Decorator_1;
+
+// Базовый декоратор
+/*
+* Базовый декоратор хранит ссылку на вложенный объект-компонент. Им может быть как конкретный компонент,
+* так и один из конкретных декораторов. Базовый декоратор делегирует все свои операции вложенному объекту.
+* Дополнительное поведение будет жить в конкретных декораторах.
+* */
+public class DataSourceDecorator implements DataSource{
+    private DataSource wrappee; // хранит ссылку на вложенный объект-компонент.
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
---
+++ src/Decorator_Wrapper/Decorator_1/EncryptionDecorator.java
@@ -0,0 +1,40 @@
+package Decorator_Wrapper.Decorator_1;
+
+import java.util.Base64;
+
+// Декоратор шифрования
+/*
+* Конкретные декораторы — это различные вариации декораторов, которые содержат добавочное поведение.
+* Оно выполняется до или после вызова аналогичного поведения обёрнутого объекта.
+* */
+public class EncryptionDecorator extends DataSourceDecorator{
+    public EncryptionDecorator(DataSource source) {
+        super(source);
+    }
+
+    @Override
+    public void writeData(String data) {
+        super.writeData(encode(data));
+    }
+
+    @Override
+    public String readData() {
+        return decode(super.readData());
+    }
+
+    private String encode(String data) {
+        byte[] result = data.getBytes();
+        for (int i = 0; i < result.length; i++) {
+            result[i] += (byte) 1;
+        }
+        return Base64.getEncoder().encodeToString(result);
+    }
+
+    private String decode(String data) {
+        byte[] result = Base64.getDecoder().decode(data);
+        for (int i = 0; i < result.length; i++) {
+            result[i] -= (byte) 1;
+        }
+        return new String(result);
+    }
+}
